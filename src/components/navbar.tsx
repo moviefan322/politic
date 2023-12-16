9cb6544c44cde0428061e8b5a46f3126968f35db
moviefan322 <philman202@yahoo.com>
2023-12-16 17:31:34 -0500

    dropdown menu functioning

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./navbar.module.css"; // Import your navbar CSS module
 import Image from "next/image";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
   return (
-    <div className={`${styles.navbar}`}>
+    <div className={styles.navbar}>
       <div className="row align-items-center justify-content-between">
         <div className="col-auto">
           <div className={styles.logo}>
@@ -14,7 +16,62 @@ const Navbar = () => {
         </div>
         <div className="col-auto">
           <div className={styles.dropdown}>
-            <GiHamburgerMenu size={24} />
+            <button
+              className={`dropdownToggle ${styles.dropdownToggle}`}
+              onClick={() => setShow(!show)}
+            >
+              <GiHamburgerMenu size={29} />
+            </button>
+            <div
+              className={`${styles.dropdownMenu} ${styles.show} text-dark d-flex flex-column justify-content-center align-items-center pt-3`}
+              aria-labelledby="dropdownMenuButton"
+            >
+              <a
+                href="#"
+                className={styles.dropdownLink}
+                style={!show ? { display: "none" } : {}}
+              >
+                Rerun
+              </a>
+              <a
+                href="#"
+                className={styles.dropdownLink}
+                style={!show ? { display: "none" } : {}}
+              >
+                Settings
+              </a>
+              <hr className="w-100 my-1" />
+              <a
+                href="#"
+                className={styles.dropdownLink}
+                style={!show ? { display: "none" } : {}}
+              >
+                Record a Screencast
+              </a>
+              <hr className="w-100 my-1" />
+              <a
+                href="#"
+                className={styles.dropdownLink}
+                style={!show ? { display: "none" } : {}}
+              >
+                Report a Bug
+              </a>
+              <a
+                href="#"
+                className={styles.dropdownLink}
+                style={!show ? { display: "none" } : {}}
+              >
+                Get Help
+              </a>
+              <hr className="w-100 my-1" />
+              <a
+                href="#"
+                className={styles.dropdownLink}
+                style={!show ? { display: "none" } : {}}
+              >
+                About
+              </a>
+            </div>
           </div>
         </div>
       </div>
