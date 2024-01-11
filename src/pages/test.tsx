f76e21ef8658bec1d9cd768e331bd2ab74647d28
moviefan322 <philman202@yahoo.com>
2024-01-11 08:39:09 -0500

    writes util chatGPT fetch function

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,9 +1,13 @@
-import HistogramChart from "@/components/histogram";
+import runChatGPT from "@/utils/runChatGpt";
 
 const test = () => {
+  const response: Promise<string> = runChatGPT("This is a test");
+
+  console.log(process.env.NEXT_PUBLIC_OPENAI_API_KEY);
+  console.log(response);
   return (
     <div>
-      <HistogramChart />
+      <h1>Test Page</h1>
     </div>
   );
 };
---
+++ src/utils/runChatGpt.ts
@@ -0,0 +1,20 @@
+export default async function runChatGPT(gptPrompt: string) {
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: gptPrompt }],
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data.choices[0].message.content;
+}
